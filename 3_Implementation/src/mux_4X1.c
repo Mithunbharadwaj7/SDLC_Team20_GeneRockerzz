a06141e74c1d138b0d08589f03db3e3559c9a695
Megha M <megzzz2000@gmail.com>
2021-08-21 13:24:13 +0530

    committed 4X1 mux

---
+++ 3_Implementation/src/mux_4X1.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include "mux.h"
+
+error_t getDataFromFile1 (mux_4x1 *_values)
+{
+    
+    FILE *fp = NULL;
+    fp = fopen ("mux_4X1.txt" , "r");
+        
+        if (fp == NULL)
+            {
+                printf("File not found!"); 
+                return FAIL;
+            }
+
+        else
+            {
+                for (int i=0; i<12; i++)
+                    {
+                        char str[100];
+                        fgets (str, 60, fp);
+                        switch (i)
+                            {
+                                case 0:
+                                    _values->_en = atoi(str);
+                                    break;
+                                case 1:
+                                    _values->_s1 = atoi(str);
+                                    break;
+                                case 2:
+                                    _values->_s2 = atoi(str);
+                                    break;
+                                case 3:
+                                    _values->_d0 = atoi(str);
+                                    break;
+                                case 4:
+                                    _values->_d1 = atoi(str);
+                                    break;
+                                case 5:
+                                    _values->_d2 = atoi(str);
+                                    break;
+                                case 6:
+                                    _values->_d3 = atoi(str);
+                                    break;
+                            }
+                    }
+            }
+
+    fclose(fp);
+
+    return 0;
+}
+
+error_t MUX_4x1 (mux_4x1 *_values)
+{
+    if(_values->_en==1)
+        {
+            if(_values->_s1==0 & _values->_s2==0)
+                {
+                    _values->_res_4x1 = _values->_d0;
+                }
+            if(_values->_s0==0 & _values->_s2==1) 
+                {
+                    _values->_res_4x1 = _values->_d1;
+                }
+            if(_values->_s0==1 & _values->_s2==0) 
+                {
+                    _values->_res_4x1 = _values->_d2;
+                }
+            if(_values->_s0==1 & _values->_s2==1) 
+                {
+                    _values->_res_4x1 = _values->_d3;
+                }
+        }
+
+    else if (~(_values->_en))
+        {
+            _values->_res_4X1 = 0;
+        }
+
+    else if (_values == NULL)
+        {
+            return FAIL;
+        }
+
+    else if (_values->_res_4x1 < 0)
+        {
+            return ERR_NEGATIVE;
+        }
+    else
+        {
+            return PASS;
+        }
+
+    return 0;
+}
