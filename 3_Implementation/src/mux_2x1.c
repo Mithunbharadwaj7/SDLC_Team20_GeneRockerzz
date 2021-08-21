3566937384b4ad16a0241e592c53e8531744054f
Megha M <megzzz2000@gmail.com>
2021-08-21 14:50:10 +0530

    renamed mux_4x1

--- 3_Implementation/src/mux_2x1.c
+++ 3_Implementation/src/mux_2x1.c
@@ -22,35 +22,11 @@ error_t getDataFromFile1 (mux_2x1 *_values){
             case 1:
                 _values->_s1 = atoi(str);
                 break;
-            case 2:
-                _values->_s2 = atoi(str);
-                break;
-            case 3:
-                _values->_s3 = atoi(str);
-                break;
             case 4:
                 _values->_d0 = atoi(str);
                 break;
             case 5:
                 _values->_d1 = atoi(str);
-                break;
-            case 6:
-                _values->_d2 = atoi(str);
-                break;
-            case 7:
-                _values->_d3 = atoi(str);
-                break;
-            case 8:
-                _values->_d4 = atoi(str);
-                break;
-            case 9:
-                _values->_d5 = atoi(str);
-                break;
-            case 10:
-                _values->_d6 = atoi(str);
-                break;
-            case 11:
-                _values->_d7 = atoi(str);
                 break;
         }
     }
