07f59889a186c8fbb8fc9c613257bf22f91bec22
AchuthaVVyas <80733877+AchuthaVVyas@users.noreply.github.com>
2021-08-22 14:13:55 +0530

    Update flipflop.h

--- 3_Implementation/inc/flipflop.h
+++ 3_Implementation/inc/flipflop.h
@@ -15,6 +15,17 @@ typedef struct D_ff{
     int D;
 }D_ff;
 
+typedef struct JK_ff{
+    int clk;
+    int J;
+    int K;
+}JK_ff;
+
+typedef struct T_ff{
+    int clk;
+    int T;
+}T_ff;
+
 typedef enum error_ff{
     PASS_FF = 0,
     FAIL_FF = -1,
@@ -23,6 +34,10 @@ typedef enum error_ff{
 error_ff getDataFromSR_ff (SR_ff *flipflop);
 error_ff getDataFromD_ff (D_ff *flipflop);
 error_ff SR_FF (SR_ff *flipflop);
-error_ff D_FF (D_ff *flipflop); 
+error_ff D_FF (D_ff *flipflop);
+error_ff getDataFromJK_ff (JK_ff *flipflop);
+error_ff getDataFromT_ff (T_ff *flipflop);
+error_ff JK_FF (JK_ff *flipflop);
+error_ff T_FF (T_ff *flipflop);
 
 #endif
