8cc3e96e9ff1764845746ba916bd62feec16794d
Lalitha74 <86546222+Lalitha74@users.noreply.github.com>
2021-08-22 12:46:38 +0530

    Update up_down counter.c

--- 3_Implementation/src/up_down counter.c
+++ 3_Implementation/src/up_down counter.c
@@ -1,1 +1,39 @@
+#include "counter.h"
 
+error_sct getDataFromCounter(count *values){
+    FILE *fp = NULL;
+    fp = fopen ("counter.txt" , "r");
+    if (fp == NULL)
+    {
+       printf("File not found!");
+       return FAIL_CT;
+    }
+    else
+    {
+    for (int i=0; i<1; i++){
+        char str[100];
+        fgets (str, 60, fp);
+        switch (i)
+        {
+            case 0:
+                values->n = atoi(str);
+                break;
+        }
+    }
+    fclose(fp);
+    }
+    return 0;
+}
+
+error_sct up_down_counter (count *values){
+
+    for(int i=0;i<values->n;i++)
+    {
+        printf("%ld\n",DTB(i));
+    }
+
+    for(int i=values->n;i>0;i--)
+    {
+        printf("%ld\n",DTB(i));
+    }
+}
