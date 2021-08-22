94be0cb77e208da3320da81e81cb47ba40297046
AchuthaVVyas <80733877+AchuthaVVyas@users.noreply.github.com>
2021-08-22 14:11:25 +0530

    Update flipflop.c
    added JK and T flipflops

--- 3_Implementation/src/flipflop.c
+++ 3_Implementation/src/flipflop.c
@@ -113,3 +113,117 @@ error_ff D_FF(D_ff *flipflop)
     }
   return 0;
 }
+
+error_ff getDataFromJK_ff (JK_ff *flipflop)
+{
+    FILE *fp = NULL;
+    fp = fopen ("Subtractor.txt" , "r");
+    if (fp == NULL)
+    {
+       printf("File not found!");
+       return FAIL_FF;
+    }
+    else
+    {
+        for (int i=0; i<3; i++)
+        {
+            char str[100];
+            fgets (str, 60, fp);
+            switch (i)
+            {
+                case 0:
+                    flipflop->clk = atoi(str);
+                    break;
+                case 1:
+                    flipflop->J = atoi(str);
+                    break;
+                case 2:
+                    flipflop->K = atoi(str);
+                    break;
+            }
+        }
+        fclose(fp);
+    }
+    return 0;
+}
+
+error_ff getDataFromT_ff (T_ff *flipflop)
+{
+    FILE *fp = NULL;
+    fp = fopen ("Subtractor.txt" , "r");
+    if (fp == NULL)
+    {
+       printf("File not found!");
+       return FAIL_FF;
+    }
+    else
+    {
+        for (int i=0; i<2; i++)
+        {
+            char str[100];
+            fgets (str, 60, fp);
+            switch (i)
+            {
+                case 0:
+                    flipflop->clk = atoi(str);
+                    break;
+                case 1:
+                    flipflop->T = atoi(str);
+                    break;
+            }
+        }
+        fclose(fp);
+    }
+    return 0;
+}
+
+error_ff JK_FF(JK_ff *flipflop)
+{
+    if(flipflop->clk == 1){
+        if(flipflop->J == 0 & flipflop->K == 0) {
+            printf("Initial State\n");
+        }
+        else if(flipflop->J == 0 & flipflop->K == 1){
+            printf("Reset State\n");
+        }
+        else if(flipflop->J == 1 & flipflop->K == 0){
+            printf("Set State\n");
+        }
+        else{
+            printf("Invalid State/Toggle State\n");
+        }
+    } else {
+        printf("No Change\n");
+    }
+    if (flipflop == NULL)
+    {
+        return FAIL_FF;
+    }
+    else
+    {
+        return PASS_FF;
+    }
+  return 0;
+}
+
+error_ff T_FF(T_ff *flipflop)
+{
+    if(flipflop->clk == 1) {
+        if(flipflop->T == 0){
+            printf("In Memory\n");
+        } else {
+            printf("Toggle\n");
+        }
+    } else {
+        printf("No change\n");
+    }
+    if (flipflop == NULL)
+    {
+        return FAIL_FF;
+    }
+    else
+    {
+        return PASS_FF;
+    }
+  return 0;
+}
