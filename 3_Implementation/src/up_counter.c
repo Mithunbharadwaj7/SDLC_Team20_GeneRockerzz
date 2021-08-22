3256e6ed079776bd420cc1e8e3240e1bf146caec
Megha M <megzzz2000@gmail.com>
2021-08-22 12:38:08 +0530

    edited up_counter.c

--- 3_Implementation/src/up_counter.c
+++ 3_Implementation/src/up_counter.c
@@ -27,7 +27,7 @@ error_sct getDataFromCounter(count *values)
     return 0;
 }
 
-error_sct up_counter (count *values)
+error_sct up_counter(count *values)
     {
         for(int i=0;i<values->n;i++)
         {
